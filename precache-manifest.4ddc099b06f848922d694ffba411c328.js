7064733dc9b1952288d04f65b4ec68fbfc5af765
Anabel <anabelpazb@gmail.com>
2019-04-24 01:00:09 -0400

    Updates

---
+++ precache-manifest.4ddc099b06f848922d694ffba411c328.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69ecda25881826061971",
+    "url": "/Drum-Machine/static/js/runtime~main.2d2b62a3.js"
+  },
+  {
+    "revision": "8cebcbe8fca5db427360",
+    "url": "/Drum-Machine/static/js/main.e2017a96.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Drum-Machine/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8cebcbe8fca5db427360",
+    "url": "/Drum-Machine/static/css/main.1d1b97fd.chunk.css"
+  },
+  {
+    "revision": "ad9912d6d777623f1dabbe79c82ebff9",
+    "url": "/Drum-Machine/index.html"
+  }
+];
