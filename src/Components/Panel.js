f33ad352929470a09e2d35d18f96e7880c395868
Anabel <anabelpazb@gmail.com>
2019-04-24 00:45:19 -0400

    mejora de la estructura del panel de controles, y agregada insignia Github

--- src/Components/Panel.js
+++ src/Components/Panel.js
@@ -1,23 +1,46 @@
-import React  from "react";
+import React from "react";
 import "../css/Panel.css";
-const Panel = (props) => {
-    return (
-        <div id="display">
-        {props.display}
-        <p>Volume:</p>
-        <input
-          type="range"
-          min={0}
-          max={1}
-          step={0.1}
-          value={props.volumen}
-          onChange={props.volumenChange}
-        />
-        <p>Power</p>
-        <input type="checkbox" checked={props.power} onChange={props.powerChange}/>
-        <p>Switch</p>
-        <input type="checkbox" onChange={props.switch} ></input>
-        </div>
-    );
-}
+const Panel = props => {
+	return (
+		<div id="display">
+			<p className="display-panel">{props.display}</p>
+			<div className="volume-panel">
+				<input
+					type="range"
+					min={0}
+					max={1}
+					step={0.1}
+					value={props.volumen}
+					onChange={props.volumenChange}
+					className="volume-input"
+				/>
+				<p>Volume</p>
+			</div>
+			<div className="switch-panel">
+				<p>Switch</p>
+				<label className="switch">
+					<input type="checkbox" onChange={props.switch} />
+					<span className="slider" />
+				</label>
+			</div>
+			<div className="power-panel">
+				<p>Power</p>
+				<label className="switch">
+					<input
+						type="checkbox"
+						checked={props.power}
+						onChange={props.powerChange}
+					/>
+					<span className="slider" />
+				</label>
+			</div>
+			<div className="gh-item">
+				by{" "}
+				<a href="https://github.com/Belquira">
+					belquira <i className="fab fa-github-alt" />
+				</a>
+			</div>
+		</div>
+	);
+};
 export default Panel;
